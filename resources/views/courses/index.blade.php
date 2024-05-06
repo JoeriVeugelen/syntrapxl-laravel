aea65f21f8ea353717c048711fb1f20e17194987
JoeriVeugelenPXL <joeri.veugelen@icloud.com>
2024-05-06 10:00:22 +0200

    Added tailwind

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -1,9 +1,13 @@
-<h1>Courses</h1>
+@extends('layouts.app')
 
-@foreach ($courses as $course)
-    <div>
-        <h2>{{ $course->title }}</h2>
-        <p>{{ $course->teaser }}</p>
-        <p class="text-red-500">{{$course->price_incl}}</p>
-    </div>
-@endforeach
+@section('content')
+    <h1>Courses</h1>
+
+    @foreach ($courses as $course)
+        <div>
+            <h2>{{ $course->title }}</h2>
+            <p>{{ $course->teaser }}</p>
+            <p class="text-red-500">{{$course->price_incl}}</p>
+        </div>
+    @endforeach
+@endsection
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <title>{{ config('app.name', 'Syntra PXL') }}</title>
+
+    <!-- Styles -->
+    @vite('resources/css/app.css')
+</head>
+<body>
+    <div id="app">
+        <!-- Here you might include a navigation bar -->
+
+        <main class="py-4">
+            @yield('content')
+        </main>
+    </div>
+
+    <!-- Scripts -->
+    <script src="{{ asset('js/app.js') }}" defer></script>
+</body>
+</html>
